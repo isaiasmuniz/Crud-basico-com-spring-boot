9943bc9871ffea00ba7ea97e3cd9ad9c8d5384b4
unknown <isaiasmuniz8@gmail.com>
2025-10-26 11:18:24 -0300

    atualizando ddados divergentes

--- src/main/resources/db/migration/V1__Create_Table_Person.sql
+++ src/main/resources/db/migration/V1__Create_Table_Person.sql
@@ -1,6 +1,7 @@
-CREATE TABLE IF NOT EXISTS personV1 (
+CREATE TABLE IF NOT EXISTS basiccrud (
     id BIGINT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	first_name  varchar(80),
 	last_name  varchar(80),
-	gender  varchar(80)
+	gender  varchar(80),
+	age int(100)
 );
